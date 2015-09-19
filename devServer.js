9de3696a6253be9fef55347c6dbfdae53bc81063
Dan Abramov <dan.abramov@gmail.com>
2015-09-19 20:20:39 +0300

    Add build process and update deps

--- devServer.js
+++ devServer.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
-var config = require('./webpack.config');
+var config = require('./webpack.config.dev');
 
 var app = express();
 var compiler = webpack(config);
