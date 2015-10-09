757f2efe7df9d32cf6d6e1e32b1eca8ca1976d54
Zill Ding <zilldzy@gmail.com>
2015-10-09 15:21:01 +0800

    Upgrade react to v0.14

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render } from 'react-dom';
 import { App } from './App';
 
-React.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
