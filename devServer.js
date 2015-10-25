bf30ec6863ecd2b3b9d86372bcabb543ab2fe3f7
Boris Yankov <borisyankov@gmail.com>
2015-10-26 01:56:56 +0200

    Serve files from directory '/public' as static resources

--- devServer.js
+++ devServer.js
@@ -13,6 +13,8 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
+app.use('/public', express.static('public'));
+
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
