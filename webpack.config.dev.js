76ac95e5fb7e4a31afc61655c8db4174ebb72aa7
Dan Abramov <dan.abramov@gmail.com>
2015-12-24 20:44:38 +0000

    Update webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    'eventsource-polyfill',
+    'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client',
     './src/index'
   ],
