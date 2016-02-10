caf1f700c8fa3cacfb625961cb0127bbcaa002ed
Steffen Kolmer <steffen@kolmer.net>
2016-02-10 16:50:51 +0100

    Updated configurations to work with the latest version of chrome debugger and webpack-hot-middleware

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,26 +10,16 @@ module.exports = {
     './src/index'
   ],
   output: {
-    devtoolModuleFilenameTemplate: function(info){
-      if(info.absoluteResourcePath.charAt(0) === '/') {
-        return 'file://'+info.absoluteResourcePath;
-      } else {
-        return 'file:///'+info.absoluteResourcePath;
-      }      
-    },
-    devtoolFallbackModuleFilenameTemplate: function(info){
-      if(info.absoluteResourcePath.charAt(0) === '/') {
-        return 'file://'+info.absoluteResourcePath+'?'+info.hash;
-      } else {
-        return 'file:///'+info.absoluteResourcePath+'?'+info.hash;
-      }      
-    },
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html', // Load a custom template 
+      inject: 'body' // Inject all scripts into the body 
+    })
   ],
   module: {
     loaders: [{
