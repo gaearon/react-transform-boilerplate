1665f38522afb1a13351ccc8ba14a0b7a4d1f7ec
xingkui wang <bit3725@gmail.com>
2016-02-27 23:12:20 +0800

    Update webpack.config.prod.js
    fix a typo

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,7 +12,7 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
