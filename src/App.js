090c4a803fb80e678dd70d5bf09a5add8264634c
steffenk <steffen@kolmer.net>
2016-03-18 20:01:40 +0100

    Moved typings to root

--- src/App.js
+++ src/App.js
@@ -2,38 +2,38 @@ import React, { Component } from 'react';
 import { NICE, SUPER_NICE } from './colors';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 };
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
 
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
-  }
+    tick() {
+        this.setState({
+            counter: this.state.counter + this.props.increment
+        });
+    }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
 
-  render() {
-    return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    );
-  }
+    render() {
+        return (
+            <h1 style={{ color: this.props.color }}>
+                Counter ({this.props.increment}): {this.state.counter}
+            </h1>
+        );
+    }
 }
 
 export class App extends Component {
-  render() {
-    return (
-      <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Counter increment={1} color={NICE} />
+                <Counter increment={5} color={SUPER_NICE} />
+            </div>
+        );
+    }
 }
