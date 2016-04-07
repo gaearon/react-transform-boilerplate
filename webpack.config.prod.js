112dec9e1cc9b47ac9b3b52711a3ecedd7579093
Steffen Kolmer <steffen@kolmer.net>
2016-04-07 19:46:33 +0200

    Fixed production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,6 +12,11 @@ module.exports = {
     },
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
         new webpack.optimize.UglifyJsPlugin({
             compressor: {
                 warnings: false
@@ -19,7 +24,8 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: 'index.html', // Load a custom template 
-            inject: 'body' // Inject all scripts into the body 
+            inject: 'body', // Inject all scripts into the body 
+            hash: true
         })
     ],
     module: {
