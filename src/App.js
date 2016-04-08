2c1d6ab30ef6c0a4c3ecc2dbe8f78be59feb0494
Steffen Kolmer <steffen@kolmer.net>
2016-04-08 17:03:39 +0200

    Optimized linting

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
+import React, { Component } from 'react'
+import { NICE, SUPER_NICE } from './colors'
+import Counter from './Counter'
 
-class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { counter: 0 };
-        this.interval = setInterval(() => this.tick(), 1000);
-    }
-
-    tick() {
-        this.setState({
-            counter: this.state.counter + this.props.increment
-        });
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-
-    render() {
-        return (
-            <h1 style={{ color: this.props.color }}>
-                Counter ({this.props.increment}): {this.state.counter}
-            </h1>
-        );
-    }
-}
-
-export class App extends Component {
+export default class App extends Component {
     render() {
         return (
             <div>
---
+++ src/Counter.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 };
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    tick() {
+        this.setState({
+            counter: this.state.counter + this.props.increment
+        });
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return (
+            <h1 style={{ color: this.props.color }}>
+                Counter ({this.props.increment}): {this.state.counter}
+            </h1>
+        )
+    }
+}
+
+Counter.propTypes = {
+    'increment': React.PropTypes.number.isRequired,
+    'color': React.PropTypes.string.isRequired
+}
+
+export default Counter
