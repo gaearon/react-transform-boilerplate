5d6633d6b786871b502a4e8a30ee5d9d4aae2331
Steffen Kolmer <steffen@kolmer.net>
2016-05-05 15:03:21 +0200

    Fixed formatting

--- src/index.js
+++ src/index.js
@@ -5,22 +5,22 @@ import App from './App';
 
 const rootEl = document.getElementById('root');
 render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
-  rootEl
+    <AppContainer>
+        <App />
+    </AppContainer>,
+    rootEl
 );
 
 if (module.hot) {
-  module.hot.accept('./App', () => {
-    // If you use Webpack 2 in ES modules mode, you can
-    // use <App /> here rather than require() a <NextApp />.
-    const NextApp = require('./App').default;
-    render(
-      <AppContainer>
-         <NextApp />
-      </AppContainer>,
-      rootEl
-    );
-  });
+    module.hot.accept('./App', () => {
+        // If you use Webpack 2 in ES modules mode, you can
+        // use <App /> here rather than require() a <NextApp />.
+        const NextApp = require('./App').default;
+        render(
+            <AppContainer>
+                <NextApp />
+            </AppContainer>,
+            rootEl
+        );
+    });
 }
