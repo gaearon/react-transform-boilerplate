c9349f6c7884de0c16ffa5d4313f56275acf5008
Steffen Kolmer <steffen@kolmer.net>
2016-05-05 15:11:31 +0200

    Moved hot loader config to index.dev.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 import App from './App';
 
 const rootEl = document.getElementById('root');
 render(
-    <AppContainer>
-        <App />
-    </AppContainer>,
+    <App />,
     rootEl
 );
-
-if (module.hot) {
-    module.hot.accept('./App', () => {
-        // If you use Webpack 2 in ES modules mode, you can
-        // use <App /> here rather than require() a <NextApp />.
-        const NextApp = require('./App').default;
-        render(
-            <AppContainer>
-                <NextApp />
-            </AppContainer>,
-            rootEl
-        );
-    });
-}
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,7 +8,7 @@ module.exports = {
         'webpack-dev-server/client?http://localhost:3000',
         'webpack/hot/only-dev-server',
         'react-hot-loader/patch',
-        './src/index'
+        './src/index.dev'
     ],
     output: {
         path: path.join(__dirname, 'dist'),
