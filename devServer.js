ed7a9a415cab9106cf99ceb43da7c3e0609cc594
Steffen Kolmer <steffen@kolmer.net>
2016-05-05 15:30:38 +0200

    code review

--- devServer.js
+++ devServer.js
@@ -3,19 +3,22 @@ const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config.dev');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true,
-  stats: {
-    colors: true,
-    chunks: false,
-    'errors-only': true
-  }
-}).listen(3000, 'localhost', function (err) {
-  if (err) {
-    console.log(err);
-  }
+const port = 3000;
+const host = 'localhost';
 
-  console.log('Listening at localhost:3000');
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    stats: {
+        colors: true,
+        chunks: false,
+        'errors-only': true
+    }
+}).listen(port, host, function (err) {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log(`Listening at http://${host}:${port}/`);
 });
