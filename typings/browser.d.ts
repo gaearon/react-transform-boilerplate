04d9a703c21dcf464b2a20cd41d114c3c7dd61f2
steffenk <steffen@kolmer.net>
2016-05-09 15:30:47 +0200

    Updated typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,3 +1,6 @@
+/// <reference path="browser/ambient/babel-core/index.d.ts" />
 /// <reference path="browser/ambient/lodash/index.d.ts" />
+/// <reference path="browser/ambient/react-dom/index.d.ts" />
 /// <reference path="browser/ambient/react/index.d.ts" />
+/// <reference path="browser/ambient/rimraf/index.d.ts" />
 /// <reference path="browser/ambient/webpack/index.d.ts" />
