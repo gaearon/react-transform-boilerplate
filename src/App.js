00f4b01971d20be3dfaa934df7b92dc113e08ad8
steffenk <steffen@kolmer.net>
2016-05-16 16:49:01 +0200

    App is now a functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { NICE, SUPER_NICE } from './colors'
 import Counter from './Counter'
 
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <Counter increment={1} color={NICE} />
-                <Counter increment={5} color={SUPER_NICE} />
-            </div>
-        );
-    }
-}
+const App = () => (
+    <div>
+        <Counter increment={1} color={NICE} />
+        <Counter increment={5} color={SUPER_NICE} />
+    </div>  
+)
+
+export default App
